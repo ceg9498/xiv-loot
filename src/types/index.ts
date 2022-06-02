112bd72097f29b66239467fc50199ac3c49abddb
Emily Gagne <ceg9498@gmail.com>
2022-06-02 12:11:02 -0400

    Update indentation.

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,55 +2,55 @@ import { IndexDB } from '../data/IndexDB';
 
 type AppProps = {};
 type AppState = {
-  src: string,
-  sample: string,
-  sets: setType[],
-  obtained: obtainedSetType,
-  equipment: Map<string,equipType>,
-  db: IndexDB,
+	src: string,
+	sample: string,
+	sets: setType[],
+	obtained: obtainedSetType,
+	equipment: Map<string,equipType>,
+	db: IndexDB,
 };
 };
 
 type setType = {
-  id:string,
-  name:string,
-  jobAbbrev:string,
-  jobIconPath:string,
-  weapon:string,
-  offHand:string,
-  head:string,
-  body:string,
-  hands:string,
-  legs:string,
-  feet:string,
-  ears:string,
-  neck:string,
-  wrists:string,
-  fingerL:string,
-  fingerR:string,
+	id:string,
+	name:string,
+	jobAbbrev:string,
+	jobIconPath:string,
+	weapon:string,
+	offHand:string,
+	head:string,
+	body:string,
+	hands:string,
+	legs:string,
+	feet:string,
+	ears:string,
+	neck:string,
+	wrists:string,
+	fingerL:string,
+	fingerR:string,
 };
 
 type obtainedSetType = {
-  [setId:string]: obtainedItemType,
+	[setId:string]: obtainedItemType,
 }
 type obtainedItemType = {
-  [slotId:string]: boolean,
+	[slotId:string]: boolean,
 }
 
 type equipDict = {
-  [index: string]: equipType,
+	[index: string]: equipType,
 }
 
 type equipType = {
-  id:string,
-  name:string,
-  iconPath:string,
   position:string,
+	id:string,
+	name:string,
+	iconPath:string,
 }
 
 type slotType = {
-  id: string,
-  pretty: string,
+	id: string,
+	pretty: string,
 }
 
 type Dictionary = { [index:string]: string };
@@ -63,6 +63,6 @@ export type {
 	setType,
 	AppProps,
 	AppState,
-  obtainedSetType,
-  obtainedItemType,
+	obtainedSetType,
+	obtainedItemType,
 }
