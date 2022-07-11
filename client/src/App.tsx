0b01e23fe87d0a13b2b99b9d3265845ea30601df
Emily Gagne <ceg9498@gmail.com>
2022-07-11 14:32:30 -0400

    Begin UI restructure to team page.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,19 +3,19 @@ import { DisplaySet, DisplayItem } from './Components';
 import {
 	AppProps,
 	AppState,
+	setType,
 } from './types';
 import { hasKey } from './Utilities';
 import {
 	sampleIDs,
 	slots,
+	role,
 	fetchGearset,
 	fetchEquipment,
 	IndexDB
 } from './data';
 import './App.css';
-
-const homePage = '/';
-const teamPage = '/team';
+import { team } from './data/temp';
 
 class App extends React.Component<AppProps, AppState>{
 	constructor(props:any) {
@@ -28,6 +28,7 @@ class App extends React.Component<AppProps, AppState>{
 			obtained: {},
 			db: new IndexDB('xivloot', 60)
 		};
+		this.addSetBulk = this.addSetBulk.bind(this);
 		this.setSrc = this.setSrc.bind(this);
 		this.getSet = this.getSet.bind(this);
 		this.toggleHas = this.toggleHas.bind(this);
@@ -47,8 +48,12 @@ class App extends React.Component<AppProps, AppState>{
 
 	componentDidMount() {
 		console.clear();
-		this.callBackendAPI(teamPage)
-			.then(res => console.log('mount test res',res))
+		// obtain sets listed in Team Data
+		this.addSetBulk(team.members);
+		this.callBackendAPI('/team')
+			.then(res => {
+				console.log('mount test res',res);
+			})
 			.catch(err => console.error('[server error]', err));
 	}
 
@@ -59,16 +64,26 @@ class App extends React.Component<AppProps, AppState>{
 		});
 	}
 
-	async getSet() {
-		let id:string;
-		if(this.state.src === '') {
-			id = sampleIDs[this.state.sample];
-		} else {
-			id = this.state.src;
-		}
-		let set = await fetchGearset(id, this.state.db);
-		const nSets = [...this.state.sets];
-		nSets.push(set);
+	async addSetBulk(members:{setID:string, obtained:any}[]) {
+		let sets:setType[] = [];
+		let obtained = this.state.obtained;
+		for(let i = 0; i < members.length; i++) {
+			const res = await this.getSet(members[i].setID);
+			sets.push(res);
+			slots.forEach(slot => {
+				if(hasKey(res, slot.id) && res[slot.id] &&
+				!this.state.equipment.has(res[slot.id])) {
+					this.getEquip(res[slot.id]);
+				}
+			});
+			obtained[members[i].setID] = members[i].obtained;
+		};
+		this.setState({
+			sets: sets,
+			obtained: obtained,
+		}, () => {setTimeout(() => {this.forceUpdate()}, 1000);});
+
+		/*
 		let obtained = this.state.obtained;
 		obtained[set.id] = {};
 		slots.forEach(slot => {
@@ -82,6 +97,18 @@ class App extends React.Component<AppProps, AppState>{
 			sets: nSets,
 			obtained: obtained
 		}, () => {setTimeout(() => {this.forceUpdate()}, 1000);});
+		*/
+	}
+
+	async getSet(id?:string) {
+		if(!id) {
+			if(this.state.src === '') {
+				id = sampleIDs[this.state.sample];
+			} else {
+				id = this.state.src;
+			}
+		}
+		return await fetchGearset(id, this.state.db);
 	}
 
 	async getEquip(id:string) {
@@ -105,41 +132,70 @@ class App extends React.Component<AppProps, AppState>{
 		return(
 		<>
 			<nav>
-			<label>Etro ID&nbsp;
-			<input type='text' onChange={(e)=>this.setSrc(e)} value={this.state.src} />
-			</label>&nbsp;or&nbsp;
-			<label>Select a sample set&nbsp;
-				<select
-				value={this.state.sample}
-				onChange={(e)=>{this.setState({sample: e.target.value})}}>
-				<option value='PLD'>Paladin</option>
-				<option value='DRK'>Dark Knight</option>
-				<option value='WHM'>White Mage</option>
-				<option value='RPR'>Reaper</option>
-				<option value='MNK'>Monk</option>
-				<option value='MCH'>Machinist</option>
-				<option value='SMN'>Summoner</option>
-				</select></label>
-			<button onClick={this.getSet}>Add Set</button>
+				<h1>{team.name}</h1>
 			</nav>
-			{this.state.sets.map((set, id) =>
-				<DisplaySet
-					key={id}
-					setInfo={set}
-					open={this.state.sets.length === 1 ? true : false}>
-					{slots.map(slot => hasKey(set, slot.id)  && set[slot.id]
-					&& equip.has(set[slot.id]) &&
-					<DisplayItem
-						key={slot.pretty+id}
-						position={slot.pretty}
-						item={equip.get(set[slot.id])}
-						obtained={this.state.obtained[set.id][slot.id]}
-						toggleHas={(e:ChangeEvent<HTMLInputElement>) =>
-							this.toggleHas(e, set.id, slot.id)}
-						/>
+			<article>
+				<section id='tank-jobs'>
+					{this.state.sets.filter(set => role[set.jobAbbrev] === 'tank').map((set, id) =>
+						<DisplaySet
+							key={id}
+							setInfo={set}
+							open={this.state.sets.length === 1 ? true : false}>
+							{slots.map(slot => hasKey(set, slot.id)  && set[slot.id]
+							&& equip.has(set[slot.id]) &&
+							<DisplayItem
+								key={slot.pretty+id}
+								position={slot.pretty}
+								item={equip.get(set[slot.id])}
+								obtained={this.state.obtained[set.id][slot.id]}
+								toggleHas={(e:ChangeEvent<HTMLInputElement>) =>
+									this.toggleHas(e, set.id, slot.id)}
+								/>
+							)}
+						</DisplaySet>
 					)}
-				</DisplaySet>
-			)}
+				</section>
+				<section id='heal-jobs'>
+					{this.state.sets.filter(set => role[set.jobAbbrev] === 'heal').map((set, id) =>
+						<DisplaySet
+							key={id}
+							setInfo={set}
+							open={this.state.sets.length === 1 ? true : false}>
+							{slots.map(slot => hasKey(set, slot.id)  && set[slot.id]
+							&& equip.has(set[slot.id]) &&
+							<DisplayItem
+								key={slot.pretty+id}
+								position={slot.pretty}
+								item={equip.get(set[slot.id])}
+								obtained={this.state.obtained[set.id][slot.id]}
+								toggleHas={(e:ChangeEvent<HTMLInputElement>) =>
+									this.toggleHas(e, set.id, slot.id)}
+								/>
+							)}
+						</DisplaySet>
+					)}
+				</section>
+				<section id='dps-jobs'>
+					{this.state.sets.filter(set => role[set.jobAbbrev] === 'dps').map((set, id) =>
+						<DisplaySet
+							key={id}
+							setInfo={set}
+							open={this.state.sets.length === 1 ? true : false}>
+							{slots.map(slot => hasKey(set, slot.id)  && set[slot.id]
+							&& equip.has(set[slot.id]) &&
+							<DisplayItem
+								key={slot.pretty+id}
+								position={slot.pretty}
+								item={equip.get(set[slot.id])}
+								obtained={this.state.obtained[set.id][slot.id]}
+								toggleHas={(e:ChangeEvent<HTMLInputElement>) =>
+									this.toggleHas(e, set.id, slot.id)}
+								/>
+							)}
+						</DisplaySet>
+					)}
+				</section>
+			</article>
 		</>
 		);
 	}
---
+++ client/src/data/temp.ts
@@ -0,0 +1,58 @@
+import { obtainedItemType } from "../types"
+
+const emptyObtained:obtainedItemType = {
+	weapon: false,
+	offhand: false,
+	head: false,
+	body: false,
+	hands: false,
+	legs: false,
+	feet: false,
+	ears: false,
+	neck: false,
+	wrists: false,
+	fingerL: false,
+	fingerR: false
+}
+
+export const team = {
+	name: "Crit Failures",
+	id: "dummy-id0",
+	members: [
+		{
+			id: 'member0',
+			name: 'Maline',
+			discordID: null,
+			setID: '6b4b1ba5-a821-41a0-b070-b1f50e986f85',
+			obtained: emptyObtained
+		},
+		{
+			id: 'member1',
+			name: 'Kyuu',
+			discordID: null,
+			setID: 'b301e789-96da-42f2-9628-95f68345e35b',
+			obtained: emptyObtained
+		},
+		{
+			id: 'member2',
+			name: 'Tou',
+			discordID: null,
+			setID: 'dda8aef5-41e4-40b6-813c-df306e1f1cee',
+			obtained: emptyObtained
+		},
+		{
+			id: 'member3',
+			name: 'Shadow',
+			discordID: null,
+			setID: '840a5088-23fa-49c5-a12a-3731ca55b4a6',
+			obtained: emptyObtained
+		},
+		{
+			id: 'member4',
+			name: 'Ugami',
+			discordID: null,
+			setID: 'e78a29e3-1dcf-4e53-bbcf-234f33b2c831',
+			obtained: emptyObtained
+		}
+	]
+}
